1ead4f8a0f7ba49dfa225f8a2726b357e3dd7d72
Pascal Andy <pascal@firepress.org>
2017-10-17 00:32:23 -0400

    traefik-alt1 /02

--- traefik-alt1/z-archives/devcommands.sh
+++ traefik-alt1/z-archives/devcommands.sh
@@ -6,10 +6,10 @@ apk update && apk upgrade && apk add nano curl bash git wget unzip ca-certificat
 cd /root;
 git clone https://github.com/pascalandy/docker-stack-this.git;
 cd docker-stack-this;
-git checkout 1.23;
+git checkout 1.24;
 
 # Go to the actual project
-cd traefik-manager-noacme; echo; pwd; echo; ls -AlhF;
+cd traefik-alt1; echo; pwd; echo; ls -AlhF;
 # Make scripts executable
 chmod +x runup; chmod +x rundown; chmod +x runctop;
 # Run the stack
