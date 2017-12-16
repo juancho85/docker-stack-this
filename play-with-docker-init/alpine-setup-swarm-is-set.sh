13d0abdba36b52044f7767de7d96968fae38f06a
Pascal Andy <pascal@firepress.org>
2017-12-16 13:01:31 -0500

    stack4 /12

--- play-with-docker-init/alpine-setup-swarm-is-set.sh
+++ play-with-docker-init/alpine-setup-swarm-is-set.sh
@@ -21,5 +21,7 @@ rm -rf /var/cache/apk/* && sleep 1                                  && \
 cd /root                                                            && \
 git clone https://github.com/pascalandy/docker-stack-this.git       && \
 cd docker-stack-this                                                && \
-echo; echo "The host is setup"                                      && \
-echo "Time to select the mono repo: "; echo                         ;
+echo                                                                && \
+echo "The host is setup"                                            && \
+echo "Time to select the mono repo: "                               && \
+echo                                                                ;
