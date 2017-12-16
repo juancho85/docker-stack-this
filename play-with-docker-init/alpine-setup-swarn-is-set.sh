2ea56322813014362fd83840abb1b5ea8065050b
Pascal Andy <pascal@firepress.org>
2017-12-16 12:51:43 -0500

    stack4 /06

--- play-with-docker-init/alpine-setup-swarn-is-set.sh
+++ play-with-docker-init/alpine-setup-swarn-is-set.sh
@@ -20,6 +20,7 @@ rm -rf /var/cache/apk/* && sleep 1                                  && \
 # Clone repo
 cd /root                                                            && \
 git clone https://github.com/pascalandy/docker-stack-this.git       && \
-
 echo; echo "The host is setup"                                      && \
+
+#
 echo; "Time to select the mono repo: "; echo                        ;
